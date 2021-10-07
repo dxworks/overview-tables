cbef2e725b014f32e9e6d4fc3e97f1f59b3e7841
Mario Rivis <mario.rivis@endava.com>
2021-10-07 17:33:15 +0300

    Added jdeploy for deploy to Github packages

--- src/main/kotlin/org/dxworks/studio/Studio.kt
+++ src/main/kotlin/org/dxworks/studio/Studio.kt
@@ -2,15 +2,15 @@ package org.dxworks.studio
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.versionOption
 import org.dxworks.studio.init.Init
 import org.dxworks.studio.ot.Tables
-import java.nio.file.Path
 
 fun main(args: Array<String>) {
 
-    Studio().subcommands(Tables(), Init()).main(args)
+    Studio().versionOption("0.1.0", names = versionCommandArgs).subcommands(Tables(), Init()).main(args)
 }
 
-class Studio : CliktCommand() {
+class Studio : CliktCommand(name = "chronos-studio") {
     override fun run() = Unit
 }
--- src/main/kotlin/org/dxworks/studio/Utils.kt
+++ src/main/kotlin/org/dxworks/studio/Utils.kt
@@ -3,6 +3,8 @@ package org.dxworks.studio
 import java.io.File
 import java.nio.file.Path
 
+val versionCommandArgs = setOf("-v", "version", "--version", "-version", "-V")
+
 fun writeDefaultConfigFile(resourcePath: String, targetFile: File) =
     object {}::class.java.classLoader.getResourceAsStream(resourcePath)
         ?.let { targetFile.writeBytes(it.readAllBytes()) }
